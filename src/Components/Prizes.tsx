b59961055df8788f438b11858bd39510e2abc5d0
Issac Roy <issacroy05@gmail.com>
2025-03-10 22:42:55 -0700

    Fix: Fixed podium heights

--- src/Components/Prizes.tsx
+++ src/Components/Prizes.tsx
@@ -8,13 +8,13 @@ const Prizes = () => {
       <h3 className="text-5xl font-bold text-white">Prizes</h3>
       <div className="flex justify-between items-end gap-[clamp(0.2rem,1vw,2.5rem)]">
         <Podium
-          hieght="23vh"
+          height="23vh"
           title="Logitech G502 HERO"
           category="Intermediate"
           image={intermediate}
         />
-        <Podium hieght="28vh" title="✨Premium Keyboard✨" category="Advanced" image={advanced} />
-        <Podium hieght="18vh" title="JBL Clip 4" category="Beginner" image={beginner} />
+        <Podium height="28vh" title="✨Premium Keyboard✨" category="Advanced" image={advanced} />
+        <Podium height="18vh" title="JBL Clip 4" category="Beginner" image={beginner} />
       </div>
       <p className="text-white text-[clamp(1rem,1.7vw,2.5rem)] text-center">
         Other prizes include Deskmats, Pop Funkos, Smiskis, and Owalas!
@@ -24,13 +24,13 @@ const Prizes = () => {
 };
 
 interface PodiumProps {
-  hieght: string;
+  height: string;
   title: string;
   category: string;
   image: string;
 }
 
-const Podium = ({ hieght, title, category, image }: PodiumProps) => {
+const Podium = ({ height, title, category, image }: PodiumProps) => {
   return (
     <div className="w-[clamp(7.5rem,20vw,30rem)] flex flex-col items-center gap-[clamp(0.2rem,1vw,2.5rem)]">
       <h4 className="text-[clamp(0.65rem,1.7vw,2.5rem)] text-white text-center">{title}</h4>
@@ -40,7 +40,8 @@ const Podium = ({ hieght, title, category, image }: PodiumProps) => {
         className="rounded-lg border-2 border-white w-full h-full object-contain"
       />
       <div
-        className={`bg-[#315654] w-full border-white border-2 rounded text-center text-white text-[clamp(1rem,1.7vw,2.5rem)] pt-4 h-[${hieght}]`}
+        className="bg-[#315654] w-full border-white border-2 rounded text-center text-white text-[clamp(1rem,1.7vw,2.5rem)] pt-4"
+        style={{ height }}
       >
         {category}
       </div>
